808489de6fccbd2f95aa815fc09f58c9a69696a9
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-02-28 13:05:00 -0500

    Standarize spacing

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -12,8 +12,8 @@ const mapping = {
 }
 
 function Square({ rank, file }: {
-    rank: '1'| '2'| '3'| '4'| '5'| '6'| '7'| '8';
-    file: 'a'| 'b'| 'c'| 'd'| 'e'| 'f'| 'g'| 'h';
+    rank: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
+    file: 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h';
 }) {
     return (
         <div className={`square square--${rank} square--${file} square--${(Number(rank) + mapping[file]) % 2 == 0? 'dark' : 'light'}`}>
