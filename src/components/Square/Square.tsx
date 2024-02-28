dd9c1e60fd8b9ff4d911a9c7e48906dc0f2af26b
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-02-28 12:50:13 -0500

    Style board squares

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,11 +1,22 @@
 import './Square.scss';
 
+const mapping = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'h': 8
+}
+
 function Square({ rank, file }: {
     rank: '1'| '2'| '3'| '4'| '5'| '6'| '7'| '8';
     file: 'a'| 'b'| 'c'| 'd'| 'e'| 'f'| 'g'| 'h';
 }) {
     return (
-        <div className={`square square--${rank} square--${file}`}>
+        <div className={`square square--${rank} square--${file} square--${(Number(rank) + mapping[file]) % 2 == 0? 'dark' : 'light'}`}>
             {file + rank}
         </div>
     );
