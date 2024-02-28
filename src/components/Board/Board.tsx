9d9519b8bf496a1f70e4990982977870a905eb36
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-02-28 12:42:38 -0500

    Create board

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,8 +1,18 @@
+import Square from '../Square/Square';
 import './Board.scss';
 
 function Board() {
+    const files: ('a'| 'b'| 'c'| 'd'| 'e'| 'f'| 'g'| 'h')[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    const ranks: ('1'| '2'| '3'| '4'| '5'| '6'| '7'| '8')[] = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
     return (
-        <></>
+        <div className='board'>
+            {ranks.map(rank => {
+                return files.map(file => {
+                    return <Square file={file} rank={rank} />
+                });
+            })}
+        </div>
     );
 }
 
--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,8 +1,13 @@
 import './Square.scss';
 
-function Square() {
+function Square({ rank, file }: {
+    rank: '1'| '2'| '3'| '4'| '5'| '6'| '7'| '8';
+    file: 'a'| 'b'| 'c'| 'd'| 'e'| 'f'| 'g'| 'h';
+}) {
     return (
-        <></>
+        <div className={`square square--${rank} square--${file}`}>
+            {file + rank}
+        </div>
     );
 }
 
